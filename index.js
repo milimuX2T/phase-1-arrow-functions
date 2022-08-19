8624850f8d3dd35d357532779d36749b22cba2bc
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-19 12:53:12 +0300

    Lab done

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function () {
+    const square = ()=>{
+        return a*a
+    }
+    return 2000/100;
+ }
+ 
+ 
+ const square = (a)=>{
+    return a*a
+}
+
+const add = (x,y)=>{
+    return x + y
+}
